7f4602bd9f2479e6a5bb19067c4bd33823f8d80a
eduardolirainfo <eduardolirainfo@gmail.com>
2020-03-26 20:26:40 -0300

    config

--- gatsby-config.js
+++ gatsby-config.js
@@ -22,8 +22,6 @@ module.exports = {
       },
     },
     `gatsby-plugin-react-helmet`,
-    `gatsby-plugin-netlify-cms`,
-
     `gatsby-plugin-netlify`,
 
     // needs to be the first to work with gatsby-remark-images
@@ -171,5 +169,6 @@ module.exports = {
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     `gatsby-plugin-offline`,
+    `gatsby-plugin-netlify-cms`,
   ],
 }
