e34ede9a5d19acf1faf28fb21d6e72f9477408e8
eduardolirainfo <eduardolirainfo@gmail.com>
2021-10-14 22:11:35 -0300

    fix perfil avatar

--- src/components/Avatar/index.js
+++ src/components/Avatar/index.js
@@ -1,13 +1,12 @@
 import React from 'react'
-import { StaticImage } from 'gatsby-plugin-image'
 
 import * as S from './styled'
 
 const Avatar = () => {
   return (
     <S.AvatarWrapper>
-      <StaticImage
-        src='../../../static/assets/img/dudulirafront.jpg'
+      <img
+        src='assets/img/dudulirafront.jpg'
         alt='Eduardo Lira'
         placeholder='blurred'
       />
